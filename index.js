da76ac0b555774a386f6d6dbfeb05e0fd7b86ddc
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-11-03 20:51:01 -0400

    commit tree 0620f00c53f1ee3c70a82fadc3b392d5396147df

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; 
+}
+
+// function saturdayFun(activity = "bathe my dog.") {
+//     return `This Saturday, I want to ${activity}`
+// }
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(character='*') {
+    let phrase1 = "You are"
+        return function(character) {
+            let phrase2 = "special"
+                return function(character) {
+                    `${phrase1} ${phrase2}`
+                }
+        }
+}
